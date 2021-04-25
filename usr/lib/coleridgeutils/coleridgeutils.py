c1c764303dcce06c4364d299de0245e3fb07c32a
pierre-si <62605527+pierre-si@users.noreply.github.com>
2021-04-25 17:09:40 +0200

    add jaccard

--- usr/lib/coleridgeutils/coleridgeutils.py
+++ usr/lib/coleridgeutils/coleridgeutils.py
@@ -100,3 +100,11 @@ def predictions_to_submission(sentences_path, predictions_path):
     )
     df = df[["PredictionString"]]
     df.to_csv("submission.csv")
+
+
+#%%
+def jaccard(str1, str2):
+    a = set(str1.lower().split())
+    b = set(str2.lower().split())
+    c = a.intersection(b)
+    return float(len(c)) / (len(a) + len(b) - len(c))
