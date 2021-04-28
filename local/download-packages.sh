e2a0dc3e93d14d244703d3f9f95106483933ea88
pierre-si <62605527+pierre-si@users.noreply.github.com>
2021-04-28 20:12:22 +0200

    fix language model installation

--- local/download-packages.sh
+++ local/download-packages.sh
@@ -11,3 +11,5 @@ pip download -d spacy spacy
 cd spacy
 pip wheel smart_open-3.0.0.tar.gz
 rm smart_open-3.0.0.tar.gz
+# other language models at https://github.com/explosion/spacy-models/releases
+wget https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.0.0/en_core_web_sm-3.0.0-py3-none-any.whl
