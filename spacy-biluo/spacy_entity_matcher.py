deabee62b8610f468188220ce85ef80e6f0db3e5
pierre-si <62605527+pierre-si@users.noreply.github.com>
2021-05-07 01:29:32 +0200

    adapt entity matcher to kaggle

--- spacy-biluo/spacy_entity_matcher.py
+++ spacy-biluo/spacy_entity_matcher.py
@@ -1,4 +1,27 @@
 #%%
+import subprocess, sys, os
+
+
+def upgrade(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-U", package])
+
+
+loc = os.environ.get("KAGGLE_KERNEL_RUN_TYPE", "Localhost")
+config = {}
+if loc == "Batch":
+    config["path_to_csv"] = "../input/coleridgeinitiative-show-us-the-data/train.csv"
+    config[
+        "path_to_publications"
+    ] = "../input/coleridgeinitiative-show-us-the-data/train"
+    upgrade("spacy")
+    subprocess.check_call([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
+
+else:
+    config["path_to_csv"] = "../input/subset_pub-split/data/val.csv"
+    config["path_to_publications"] = "../input/subset_pub-split/data/val/"
+
+
+#%%
 import json
 from pathlib import Path
 
@@ -74,35 +97,36 @@ def publication_biluo(publication_path, dataset_labels):
 
 
 # %%
-df = pd.read_csv("../input/subset_pub-split/data/val.csv")
-publications = Path("../input/subset_pub-split/data/val/").iterdir()
-# %%
-for p in tqdm(publications):
-    labels = df[df["Id"] == p.stem]["dataset_label"].values
-    output = publication_iob(p, labels)
-    p_df = pd.DataFrame(output)
-    p_df.to_json(
-        "../input/smaller/iob2/train/" + p.stem + ".jsonl", orient="records", lines=True
-    )
-# %%
-for p in tqdm(publications):
-    labels = np.unique(
-        df[df["Id"] == p.stem][["dataset_title", "dataset_label"]].values.ravel()
-    )
-    output = publication_biluo(p, labels)
-    p_df = pd.DataFrame(output)
+def process_folder(folder_path, train_df):
+    publications = Path(folder_path).iterdir()
 
-    p_df.to_json(
-        "../input/subset_pub-split/biluo/val/" + p.stem + ".jsonl",
-        orient="records",
-        lines=True,
-    )
+    global_df = pd.DataFrame()
+    for p in publications:
+        labels = np.unique(
+            df[df["Id"] == p.stem][["dataset_title", "dataset_label"]].values.ravel()
+        )
+        output = publication_biluo(p, labels)
+        p_df = pd.DataFrame(output)
+        global_df = global_df.append(p_df)
 
+        output_folder = Path(folder_path).name
+        Path(output_folder).mkdir(exist_ok=True)
+        p_df.to_json(
+            output_folder + "/" + p.stem + ".jsonl",
+            orient="records",
+            lines=True,
+        )
+    global_df.to_json(output_folder + ".json", orient="records", lines=True)
+
+
+#%%
+df = pd.read_csv(config["path_to_csv"])
+process_folder(config["path_to_publications"], df)
 # %% COMPARISON WITH BERT PRETOKENIZER
-from tokenizers.pre_tokenizers import BertPreTokenizer
+# from tokenizers.pre_tokenizers import BertPreTokenizer
 
-pretokenizer = BertPreTokenizer()
-# pretokenizer.pre_tokenize_str('All told, the annual, in-state cost of attendance at a public four-year institution will run about $38,000 in the early 2030s in today\'s dollars')
-tokenized = pretokenizer.pre_tokenize_str(content[0]["text"])
-print("Spacy:", len(doc), "Hugginface:", len(tokenized))
-# Huggingface splits numbers with a comma whereas Spacy does not.
+# pretokenizer = BertPreTokenizer()
+# # pretokenizer.pre_tokenize_str('All told, the annual, in-state cost of attendance at a public four-year institution will run about $38,000 in the early 2030s in today\'s dollars')
+# tokenized = pretokenizer.pre_tokenize_str(content[0]["text"])
+# print("Spacy:", len(doc), "Hugginface:", len(tokenized))
+# # Huggingface splits numbers with a comma whereas Spacy does not.
